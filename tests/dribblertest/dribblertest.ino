5e741f1ba7eab0a2b196888475e3313ea8952c55
Guang-Lin Wei <guanglinwei@gmail.com>
2021-01-09 19:27:25 -0500

    dribble test

--- tests/dribblertest/dribblertest.ino
+++ tests/dribblertest/dribblertest.ino
@@ -9,12 +9,26 @@ float val = 0.0;
 void setup() {
   // put your setup code here, to run once:
   ESC.attach(PIN, MIN_PULSE, MAX_PULSE);
+  ESC.write(0);
+  delay(2000);
+
+  for(int x = 0; x <= 100; x += 10) {
+    ESC.write(x);
+    delay(500);
+  }
+
+  for(int x = 100; x > 0; x -= 10) {
+    ESC.write(x);
+    delay(500);
+  }
+
+  ESC.write(0);
+  delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 //  pot = analogRead(A0);
 //  pot = map(pot, 
-  ESC.write(val);
-  val += 0.5;
+  
 }
