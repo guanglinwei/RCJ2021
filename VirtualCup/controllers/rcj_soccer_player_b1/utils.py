1e96ef83fcf594144f46e73ab091e8fd7e747d37
Peter Molzer <pete.molzer@gmail.com>
2021-01-22 20:50:17 -0500

    stuff

--- VirtualCup/controllers/rcj_soccer_player_b1/utils.py
+++ VirtualCup/controllers/rcj_soccer_player_b1/utils.py
@@ -1,3 +1,4 @@
+from math import *
 def get_direction(ball_angle: float) -> int:
     """Get direction to navigate robot to face the ball
 
@@ -11,3 +12,12 @@ def get_direction(ball_angle: float) -> int:
         return 0
     return -1 if ball_angle < 180 else 1
 
+def get_distance(dist1: dict, dist2: dict):
+    x1 = dist1["x"]
+    x2 = dist2["x"]
+    y1 = dist1["y"]
+    y2 = dist2["y"]
+
+    distance = abs(sqrt((x2-x1)**2+(y2-y1)**2))
+    return distance
+        
--- libraries/HoloMove/HoloMove.h
+++ libraries/HoloMove/HoloMove.h
@@ -7,7 +7,7 @@
 class HoloMove
 {
 	public:
-		HoloMove(const RoboClaw* rb, HardwareSerial &ser);
+		HoloMove(RoboClaw* rb, HardwareSerial &ser);
 		void move(float direction, float speed = 1, float rotation = 0, float *a = 0, float *b = 0, float *c = 0, float *d = 0);
 	private:
 		RoboClaw* _rb;
