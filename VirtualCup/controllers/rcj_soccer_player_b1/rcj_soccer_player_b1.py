822391fb58154152e91e5d697b33e59c0537b136
Peter Molzer <pete.molzer@gmail.com>
2021-02-09 17:54:03 -0500

    Update rcj_soccer_player_b1.py

--- VirtualCup/controllers/rcj_soccer_player_b1/rcj_soccer_player_b1.py
+++ VirtualCup/controllers/rcj_soccer_player_b1/rcj_soccer_player_b1.py
@@ -7,7 +7,7 @@
 from math import *
 from utils import *
 import random
-
+import time
 # You can also import scripts that you put into the folder with controller
 from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
 
@@ -21,9 +21,14 @@ class MyRobot(RCJSoccerRobot):
         gap = .1
         global degDirection
         self.ball = 0
-        self.stop = True
+        self.stop = False
         
-        self.isPrinting = True
+        self.isPrinting = False
+        self.reversing = False
+        
+        self.goalie = False;
+        self.attacker = True;
+        self.annoyer = False;
 
         while self.robot.step(TIME_STEP) != -1:
 
@@ -78,40 +83,56 @@ class MyRobot(RCJSoccerRobot):
                 
                 # Get angle between the robot and the ball
                 # and between the robot and the north
-                ball_angle, robot_angle = self.get_angles(ball_pos, self.robot_pos)
-                #print('Ball: ' + str(ball_angle))
-                #print("Robot B: " + str(robot_angle))
-
-                # Compute the speed for motors
-                goalAngle, nothing = self.get_angles(self.enemyGoalPos(), self.robot_pos)
-
-                targetPos, isBackward = self.avoid(data,targetPos)
-
-                if self.isStuckOnWall(data):
-                    self.goBackwards(10,True)
-                elif isBackward:
-                    self.goBackwards(8)
-                    self.myprint("Going Backwards")
+                if self.attacker:
+                    ball_angle, robot_angle = self.get_angles(ball_pos, self.robot_pos)
+                    #print('Ball: ' + str(ball_angle))
+                    #print("Robot B: " + str(robot_angle))
+    
+                    # Compute the speed for motors
+                    goalAngle, nothing = self.get_angles(self.enemyGoalPos(), self.robot_pos)
+    
+                    targetPos, isBackward = self.avoid(data,targetPos)
                     
-                elif get_distance(self.robot_pos,targetPos) <=0.1:
-                    if abs(goalAngle-ball_angle) <= 20:
-
+                    if self.isStuckOnWall(data):
+                        self.reversing = True
+                    elif isBackward:
+                        self.goBackwards(8)
+                        self.myprint("Going Backwards")
+                        
+                    elif get_distance(self.robot_pos,targetPos) <=0.1:
+                        #if abs(goalAngle-ball_angle) <= 20:
+    
                         self.goto(self.enemyGoalPos(),self.robot_pos)
                         self.myprint("Going to Goal")
+                        #else:
+                            #direction = get_direction(goalAngle)
+                            #self.rotate(direction)
+                            #self.myprint("Turning to goal")
+    
                     else:
-                        direction = get_direction(goalAngle)
-                        self.rotate(direction)
-                        self.myprint("Turning to goal")
-
-                else:
-
-                    #self.goto(self.targetPos(ball_pos),robot_pos)
-                    self.myprint("Going to target")
-                    self.goto(targetPos,self.robot_pos)
-                # If the robot has the ball right in front of it, go forward,
-                # rotate otherwise
+    
+                        #self.goto(self.targetPos(ball_pos),robot_pos)
+                        self.myprint("Going to target")
+                        self.goto(targetPos,self.robot_pos)
+                    # If the robot has the ball right in front of it, go forward,
+                    # rotate otherwise
+                    
+                    self.ball = ball_pos
+                    
+                    if self.reversing:
+                        self.goBackwards(10,True)
+                        time.sleep(2)
+                        self.reversing = False
+                        
+                elif self.goalie:
+                    targetPos = self.goalieTarget(ball_pos)
+                    self.gotoGoalie(targetPos, self.robot_pos)
                 
-                self.ball = ball_pos
+                elif self.annoyer:
+                    self.annoy(data)
+                
+                
+                
                 
                 if self.stop:
                     self.left_motor.setVelocity(0)
@@ -122,8 +143,8 @@ class MyRobot(RCJSoccerRobot):
         if direction == 0:
             self.goto(self.enemyGoalPos(), self.robot_pos)
         elif direction == 1:
-            self.left_motor.setVelocity(5*direction)
-            self.right_motor.setVelocity(10*direction)
+            self.left_motor.setVelocity(-5*direction)
+            self.right_motor.setVelocity(-10*direction)
         else:
             self.left_motor.setVelocity(10*direction)
             self.right_motor.setVelocity(5*direction)
@@ -216,25 +237,48 @@ class MyRobot(RCJSoccerRobot):
         else:
             if direction == -1:
                 left_speed = -10
-                right_speed = -3
+                right_speed = -4
             else:
-                left_speed = -3
+                left_speed = -4
                 right_speed = -10
 
         # Set the speed to motors
         self.left_motor.setVelocity(left_speed)
         self.right_motor.setVelocity(right_speed)
 
+    def gotoGoalie(self, coords, robot_pos):
+        goAngle, nothing = self.get_angles(coords, robot_pos)
+        direction = get_direction(goAngle)
+        
+            
+        if get_distance(coords, robot_pos) > .11:
+            if direction == 0:
+                left_speed = -10
+                right_speed = -10
+            else:
+                if direction == -1:
+                    left_speed = -10
+                    right_speed = 5
+                else:
+                    left_speed = 5
+                    right_speed = -10
 
+            # Set the speed to motors
+            self.left_motor.setVelocity(left_speed)
+            self.right_motor.setVelocity(right_speed)
+        else:
+            self.left_motor.setVelocity(0)
+            self.right_motor.setVelocity(0)
+    
     def goBackwards(self, curve, reallyBackward = False):
         if reallyBackward:
             self.left_motor.setVelocity(curve)
             self.right_motor.setVelocity(curve)
             return
 
-        rightS = 10
+        rightS = 9
         #leftS = random.choice([10,10-curve])
-        leftS = -10
+        leftS = 3
         if leftS == 10:
             rightS = 10-curve
 
@@ -316,7 +360,7 @@ class MyRobot(RCJSoccerRobot):
 
 
         #Stuck on robot
-        if closestDistance < .15:
+        if closestDistance < .10:
             angle, nothing = self.get_angles(enemyPos,robot_pos)
             if self.approxAngle(angle,0,70):
                 return targetPos, True
@@ -335,45 +379,84 @@ class MyRobot(RCJSoccerRobot):
 
         return targetPos, False
         
+    def annoy(self, data):
+        
+        robot_pos = data[self.name]
+        ball_pos = data["ball"]
+        
+        
+        
+        farthestEnemy = self.enemyGoal() + "1"
+        farthestDistance = 0
+        ballDistance = get_distance(robot_pos,ball_pos)
+
+        for robot in data:
+            if robot != "ball" and robot != self.name and robot[0] == self.enemyGoal():
+
+                dist = get_distance(ball_pos,data[robot])
+                if dist > farthestDistance:
+                    farthestDistance = dist
+                    farthestEnemy = robot
+        enemyPos = data[farthestEnemy]
+        angEnemy,nothing = self.get_angles(enemyPos,robot_pos)
+        
+        self.goto(enemyPos, robot_pos)
     
     def goalieTarget(self, ball_pos):
-        #self.robot_pos
         ball_pos["orientation"] = 0
-        
-        
-        
-        if self.myGoal() == "Y":
-            goalx = -0.7
-        else:
-            goalx = 0.7
-            
-        distToBall = abs(goalx - ball_pos["x"])
-        targy = ball_pos["y"]
-        targx = goalx
-        
+        goaltol = 0.68
+        targtol = .4
         
         angle, nothing = self.get_angles(self.ball, ball_pos)
-        
         if angle < 0:
             angle = 270 + (90 - abs(angle))
+         
+        if self.myGoal() == "Y":
         
-        
-        if angle > 350 or angle < 10 or (angle < 190 and angle > 170):
-            targy = ball_pos["y"]
-            targx = goalx
+            goalx = -goaltol
         else:
-            y = distToBall * tan(angle)
-            targy = ball_pos["y"] + y
-            targx = goalx
+            goalx = goaltol
         
-        if targy > 0.16:
-            targy = 0.16
-        elif targy < -0.16:
-            targy = -0.16
+        #If goalie far away from gaol
+        targx = goalx
+        if abs(self.robot_pos["x"] - targx) > .1:
+            
+            targy = 0
         
+        #If ball is far away
+        elif get_distance(ball_pos, self.robot_pos) > 0.8:
+            
+            if self.myGoal() == "Y":
+                targx += .4
+            else:
+                targx -= .4
+            
+            targy = ball_pos["y"]
+
+        #Normal case
+        else:
         
+            distToBall = abs(goalx - ball_pos["x"])
+            targy = ball_pos["y"]
+            
+            
+            if angle > 350 or angle < 10 or (angle < 190 and angle > 170):
+                targy = ball_pos["y"]
+                
+                self.myprint("Dumb")
+            else:
+                y = distToBall * tan(angle)
+                targy = ball_pos["y"] + y
+                
+                self.myprint("Intelligent Goalie")
         
-        target = {"x": targx, "y": targy, "orientation": 0} 
+        if targy > targtol:
+            targy = targtol
+        elif targy < -targtol:
+            targy = -targtol
+        target = {"x": targx, "y": targy, "orientation": 0}
+    
+
         self.myprint(target)
         return target
     
