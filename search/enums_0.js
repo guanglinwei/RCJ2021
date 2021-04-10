f0b043a556e29f2b7eb929ca10a924183f9c86de
guanglinwei <guanglinwei@users.noreply.github.com>
2021-04-10 21:56:01 +0000

    deploy: fe2a7a79702b625169c702be5e9b55b0d164ccf9

--- search/enums_0.js
+++ search/enums_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['joystickinputtype_36',['JoystickInputType',['../class_display_controller.html#a50e44d48baf8881cd6e4446c96fa7363',1,'DisplayController']]]
+  ['joystickinputtype_37',['JoystickInputType',['../class_display_controller.html#a50e44d48baf8881cd6e4446c96fa7363',1,'DisplayController']]]
 ];
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['printincurrentwindowatposition_28',['printInCurrentWindowAtPosition',['../class_display_controller.html#a1900a4967b6abe829b8e4396c41c2940',1,'DisplayController']]],
-  ['printinwindow_29',['printInWindow',['../class_display_controller.html#a4c759b2a0c45a3a4e13b8c7bfe105b95',1,'DisplayController']]],
-  ['printinwindowatposition_30',['printInWindowAtPosition',['../class_display_controller.html#aad038dbc04c43282b8ffb809dbf3099e',1,'DisplayController']]]
+  ['printincurrentwindowatposition_29',['printInCurrentWindowAtPosition',['../class_display_controller.html#a1900a4967b6abe829b8e4396c41c2940',1,'DisplayController']]],
+  ['printinwindow_30',['printInWindow',['../class_display_controller.html#a4c759b2a0c45a3a4e13b8c7bfe105b95',1,'DisplayController']]],
+  ['printinwindowatposition_31',['printInWindowAtPosition',['../class_display_controller.html#aad038dbc04c43282b8ffb809dbf3099e',1,'DisplayController']]]
 ];
