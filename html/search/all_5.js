ddc5917057199662c773698b747d0491450d7cc9
guanglinwei <guanglinwei@users.noreply.github.com>
2021-04-17 18:27:14 +0000

    deploy: 2fa19320084039f372f32df1e8d56081b9ca2e7d

--- html/search/all_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['move_7',['move',['../class_holo_move.html#add77053a38965a008cdece3cea20915f',1,'HoloMove']]]
-];
--- html/search/functions_6.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['printincurrentwindowatposition_29',['printInCurrentWindowAtPosition',['../class_display_controller.html#a1900a4967b6abe829b8e4396c41c2940',1,'DisplayController']]],
-  ['printinwindow_30',['printInWindow',['../class_display_controller.html#a4c759b2a0c45a3a4e13b8c7bfe105b95',1,'DisplayController']]],
-  ['printinwindowatposition_31',['printInWindowAtPosition',['../class_display_controller.html#aad038dbc04c43282b8ffb809dbf3099e',1,'DisplayController']]]
-];
